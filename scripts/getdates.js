a8bec74dc88044482a9e991ae9830f29efc5e630
BsauerSchool <bsauer@byupathway.edu>
2026-09-12 13:24:31 -0600

    Finished assignment 1

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+let currentYear = today.getFullYear();
+
+document.getElementById("current-year").innerHTML = `&copy;${currentYear}`;
+document.getElementById("lastModified").innerHTML = `last Modification ${document.lastModified}`;
+
